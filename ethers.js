1405dad8fbb451181d9cba34ee623cdb4900d4a3
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-06-27 19:06:58 +0530

    Delete ethers.js

--- ethers.js
+++
@@ -1,9 +0,0 @@
-// Load ethers.js dynamically if not included in HTML
-(function loadEthers() {
-    if (typeof window.ethers === "undefined") {
-        const script = document.createElement("script");
-        script.src = "https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.min.js";
-        script.onload = () => console.log("ethers.js loaded");
-        document.head.appendChild(script);
-    }
-})();
