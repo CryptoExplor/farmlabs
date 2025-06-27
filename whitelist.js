ef11fad132d9f5b6e21375d2f556a80a50ac167a
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-06-27 19:04:54 +0530

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,9 +1,3 @@
-const WHITELIST_MODE = false; // toggle this flag
-
-const REAL_WHITELIST = [
-    "0x5c5a38168517b610fe06b00c07a2d45bbb10c2e8",
-    ...
-].map(addr => addr.toLowerCase());
-
-const WHITELISTED_ADDRESSES = WHITELIST_MODE ? REAL_WHITELIST : [];
-WHITELISTED_ADDRESSES.includes = () => true;
+if (WHITELISTED_ADDRESSES.includes(userAddress.toLowerCase())) {
+  // allow access
+}
