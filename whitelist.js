456928d372bc4961daa933a64d33081a1f6133d4
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-06-28 22:24:19 +0530

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,20 +1,9 @@
 // whitelist.js
 
-// Empty whitelist array (can be filled later if needed)
-window.WHITELISTED_ADDRESSES = [].map(addr => addr.toLowerCase());
+// Whitelist array (empty, since we're allowing all users)
+window.WHITELISTED_ADDRESSES = [];
 
-// Set this flag to true to allow all users (whitelist bypass mode)
-window.ALLOW_ALL_USERS = true;
-
-/**
- * Call this function to check if a wallet is allowed.
- * If ALLOW_ALL_USERS is true, all addresses are allowed.
- * Otherwise, only those in the whitelist are allowed.
- * 
- * @param {string} address - Wallet address to check
- * @returns {boolean}
- */
-window.isWalletWhitelisted = function(address) {
-    if (window.ALLOW_ALL_USERS) return true;
-    return window.WHITELISTED_ADDRESSES.includes(address.toLowerCase());
+// Always return true for all addresses
+window.isWalletWhitelisted = function (address) {
+    return true; // All addresses are whitelisted
 };
