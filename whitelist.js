be9adc100fff50a06a397c73c50496d1f10b395e
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-06-28 22:18:38 +0530

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,3 +1,20 @@
-if (window.isWalletWhitelisted(userAddress)) {
-    // proceed
-}
+// whitelist.js
+
+// Example: Add whitelisted wallet addresses here
+window.WHITELISTED_ADDRESSES = [
+    // "0xabc...123",
+    // "0xdef...456",
+].map(addr => addr.toLowerCase());
+
+// Flag for testing - allows all addresses to be whitelisted
+window.ALLOW_ALL_WALLETS = true;
+
+/**
+ * Check if wallet is whitelisted
+ * @param {string} walletAddress
+ * @returns {boolean}
+ */
+window.isWalletWhitelisted = function(walletAddress) {
+    if (window.ALLOW_ALL_WALLETS) return true;
+    return window.WHITELISTED_ADDRESSES.includes(walletAddress.toLowerCase());
+};
