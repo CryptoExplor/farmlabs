5b4bb0a25e61f4d7c65f177721f0a66ef095a4b9
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-07-04 21:49:03 +0530

    Delete whitelist.js

--- whitelist.js
+++
@@ -1,9 +0,0 @@
-// whitelist.js
-
-// Whitelist array (empty, since we're allowing all users)
-window.WHITELISTED_ADDRESSES = [];
-
-// Always return true for all addresses
-window.isWalletWhitelisted = function (address) {
-    return true; // All addresses are whitelisted
-};
